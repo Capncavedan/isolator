f6f146970a6a76c6fca4fb2162b39fc122e4e15f
Dan Buettner <dan@danbuettner.net>
2016-08-06 09:33:34 -0500

    use a redirect after post, and allow clearing hospital number

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -6,6 +6,10 @@ Then(/^I should see "([^"]*)"$/) do |content|
   expect(page).to have_content content
 end
 
+Then(/^I should not see "([^"]*)"$/) do |content|
+  expect(page).to_not have_content content
+end
+
 When(/^I enter hospital number "([^"]*)"$/) do |num|
   fill_in "hospital_number", with: num
 end
@@ -14,8 +18,9 @@ When(/^I press "([^"]*)"$/) do |button_text|
   click_button button_text
 end
 
-Then(/^I should see a success message saying "([^"]*)"$/) do |message_text|
-  within ".flash-message.success" do
+Then(/^I should see a (success|notice|failure) flash message saying "([^"]*)"$/) do |flash_type, message_text|
+  within ".flash.flash-message.#{flash_type}" do
     expect(page).to have_content message_text
   end
 end
+
