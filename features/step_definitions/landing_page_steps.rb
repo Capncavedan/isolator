e5b0d3051c09fc637ca1f50a1d2d45a4f0d4b247
Dan Buettner <dan@danbuettner.net>
2016-08-06 09:40:17 -0500

    move generic steps to shared_step.rb file

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,26 +1,3 @@
-When(/^I go to the app's home page$/) do
-  visit "/"
-end
-
-Then(/^I should see "([^"]*)"$/) do |content|
-  expect(page).to have_content content
-end
-
-Then(/^I should not see "([^"]*)"$/) do |content|
-  expect(page).to_not have_content content
-end
-
 When(/^I enter hospital number "([^"]*)"$/) do |num|
   fill_in "hospital_number", with: num
 end
-
-When(/^I press "([^"]*)"$/) do |button_text|
-  click_button button_text
-end
-
-Then(/^I should see a (success|notice|failure) flash message saying "([^"]*)"$/) do |flash_type, message_text|
-  within ".flash.flash-message.#{flash_type}" do
-    expect(page).to have_content message_text
-  end
-end
-
