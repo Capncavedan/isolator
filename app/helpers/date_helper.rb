180fefe65a0b48c78a8f5e8fd0c801cb3a9f907a
Dan Buettner <dan@danbuettner.net>
2016-08-06 12:06:39 -0500

    correct date format issue and layout issue

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,10 +1,10 @@
 module DateHelper
 
   def full_date_format(the_date)
-    if the_date.is_a? Date
-      the_date.strftime "%Y-%m-%d %H:%M:%S %z"
+    if the_date.is_a? String
+      DateTime.parse(the_date).strftime "%Y-%m-%d %H:%M:%S %z"
     else
-      full_date_format DateTime.parse(the_date)
+      the_date.strftime "%Y-%m-%d %H:%M:%S %z"
     end
   rescue
     "-"
