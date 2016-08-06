8d091d472afde0ba91f1b0bf2e1be5751ee7728f
Dan Buettner <dan@danbuettner.net>
2016-08-06 11:37:12 -0500

    adds validations to shipment records

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,8 @@
 class Shipment < ApplicationRecord
+
+  validates :hospital_number,  presence: true
+  validates :starting_number,  presence: true
+  validates :isolate_quantity, presence: true
+  validates :objective_code,   presence: true, inclusion: {in: %w(A B Q)}
+
 end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe Shipment, type: :model do
     expect { Shipment.new }.to_not raise_error
   end
 
+  describe "validations" do
+    before do
+      @shipment = Shipment.new
+      @shipment.valid?
+    end
+
+    it "validates presence of hospital_number" do
+      expect(@shipment.errors[:hospital_number]).to include "can't be blank"
+    end
+
+    it "validates presence of objective_code" do
+      expect(@shipment.errors[:objective_code]).to include "can't be blank"
+    end
+
+    it "validates presence of starting_number" do
+      expect(@shipment.errors[:starting_number]).to include "can't be blank"
+    end
+
+    it "validates presence of isolate_quantity" do
+      expect(@shipment.errors[:isolate_quantity]).to include "can't be blank"
+    end
+
+    it "validates objective_code is one of a list" do
+      expect(@shipment.errors[:objective_code]).to include "is not included in the list"
+    end
+  end
+
 end
