5b729841594a91dbf2428623efa23d87db6e61c3
Dan Buettner <dan@danbuettner.net>
2016-08-06 19:00:56 -0500

    WIP 2

--- features/step_definitions/isolate_data_entry_steps.rb
+++ features/step_definitions/isolate_data_entry_steps.rb
@@ -3,7 +3,19 @@ Then(/^I should be on the isolate data entry form$/) do
 end
 
 When(/^I fill out the Objective\-A isolate data$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in "JIM number",               with: "1"
+  fill_in "Code number",              with: "A-1"
+  fill_in "Accession number",         with: "ABC123"
+  fill_in "Organism",                 with: "H. pylori"
+  fill_in "Date of admission",        with: "7/1/2016"
+  fill_in "Date of culture",          with: "7/2/2016"
+  select  "OTH / Other or Unknown",   from: "Source of BSI"
+  select  "M",                        from: "Sex"
+  fill_in "Age",                      with: "42"
+  select  "ID / Infectious Disease",  from: "Service"
+  select  "INF / Infection",          from: "Primary diagnosis"
+  select  "Yes",                      from: "ICU?"
+  select  "No",                       from: "Nosocomial?"
 end
 
 Then(/^I should be on the shipments index page$/) do
