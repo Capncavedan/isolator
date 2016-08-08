84384fa6f33f3e7be45d0499ef419ae54e7eb447
Dan Buettner <dan@danbuettner.net>
2016-08-08 13:52:50 -0500

    switch away from simple_form_bootstrap3 gem to bare simple_form config

--- config/initializers/simple_form.rb
+++
@@ -1,8 +0,0 @@
-SimpleForm.setup do |config|
-  config.boolean_style = :inline
-  config.button_class = 'btn btn-primary'
-  config.error_notification_tag = :div
-  config.error_notification_class = 'alert alert-danger'
-  config.browser_validations = false
-  config.default_wrapper = :default
-end
