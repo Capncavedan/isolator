79182923db871ba90f08a01243a1f7ffba562d28
Dan Buettner <dan@danbuettner.net>
2016-08-09 05:59:00 -0500

    add organism for objective B in db:seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,18 @@ hospital_numbers.each do |hospital_number|
   - starting_number = 1
   shipment_quantity.times do
     print "."
-    shipment = Shipment.create hospital_number: hospital_number,
+
+    shipment = Shipment.new hospital_number: hospital_number,
       starting_number: starting_number,
       objective_code: %w(A B Q).sample,
       isolate_quantity: 20+rand(20)
+
+    if shipment.objective_code == "B"
+      shipment.organism = ["H. influenza", "M. catarrhalis"].sample
+    end
+
+    shipment.save
+
     starting_number += shipment.isolate_quantity
   end
 end
