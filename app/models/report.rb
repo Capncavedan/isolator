b643e2f88ee3ca73621bafbee8557b95b0fe1435
Dan Buettner <dan@danbuettner.net>
2016-08-17 22:27:37 -0500

    intermediate step - group by's and still use hash

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,42 +2,42 @@ class Report
 
   def self.hospital_stats
     hsh = Hash.new(0)
+    hospital_stats_rows.each do |row|
+      hospital_number, objective_code, objective_code_shipment_count, objective_code_isolate_count = row
 
-    shipment_hospital_numbers.each do |hospital_number|
-      # shipment count for hospital
-      hsh["shipment_info_#{hospital_number}"] = Shipment.where(hospital_number: hospital_number).count
+      hsh["shipment_info_#{hospital_number}"] += objective_code_shipment_count
+      hsh["shipment_info"] += objective_code_shipment_count
 
-      # objective counts for hospital
-      %w(A B Q).each do |objective_code|
-        hsh["objective_info_#{objective_code}_#{hospital_number}"] = Shipment.where(hospital_number: hospital_number, objective_code: objective_code).count
-      end
+      hsh["objective_info_#{objective_code}_#{hospital_number}"] += objective_code_shipment_count
+      hsh["objective_info_#{objective_code}"] += objective_code_shipment_count
 
-      # isolate counts for hospital
-      %w(A B Q).each do |objective_code|
-        hsh["isolate_info_#{objective_code}_#{hospital_number}"] = Shipment.joins(:isolates).where(hospital_number: hospital_number, objective_code: objective_code).count
-      end
-      hsh["isolate_info_total_#{hospital_number}"] = Shipment.joins(:isolates).where(hospital_number: hospital_number).count
+      hsh["isolate_info_#{objective_code}_#{hospital_number}"] += objective_code_isolate_count
+      hsh["isolate_info_#{objective_code}"] += objective_code_isolate_count
+      hsh["isolate_info_total_#{hospital_number}"] += objective_code_isolate_count
+      hsh["isolate_info_total"] += objective_code_isolate_count
     end
-
-    # shipment count for ALL hospitals
-    hsh["shipment_info"] = Shipment.count
-
-    # objective counts for ALL hospitals
-    %w(A B Q).each do |objective_code|
-      hsh["objective_info_#{objective_code}"] = Shipment.where(objective_code: objective_code).count
-    end
-
-    # isolate counts for ALL hospitals
-    %w(A B Q).each do |objective_code|
-      hsh["isolate_info_#{objective_code}"] = Shipment.joins(:isolates).where(objective_code: objective_code).count
-    end
-    hsh["isolate_info_total"] = Shipment.joins(:isolates).count
-
     hsh
   end
 
   def self.shipment_hospital_numbers
     Shipment.distinct.pluck(:hospital_number).sort
+  end
+
+
+  private
+
+  def self.hospital_stats_rows
+    sql = <<-END_OF_SQL
+SELECT hospital_number,
+shipments.objective_code AS objective_code,
+COUNT(DISTINCT shipments.id) AS objective_code_shipment_count,
+COUNT(isolates.*) AS objective_code_isolate_count
+FROM shipments
+LEFT OUTER JOIN isolates ON isolates.shipment_id = shipments.id
+GROUP BY 1,2
+ORDER BY 1,2
+END_OF_SQL
+    ActiveRecord::Base.connection.select_rows sql
   end
 
 end
