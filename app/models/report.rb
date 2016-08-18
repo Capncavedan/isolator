1ae60fa814e601e9afe20b56060d6a810e14ea4b
Dan Buettner <dan@danbuettner.net>
2016-08-18 15:19:56 -0500

    Use ROLLUP variant (CUBE) to get actual summaries in results from DB

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,18 +3,24 @@ class Report
   def self.hospital_stats
     hsh = Hash.new(0)
     hospital_stats_rows.each do |row|
-      hospital_number, objective_code, objective_code_shipment_count, objective_code_isolate_count = row
+      hospital_number, objective_code, shipment_count, isolate_count = row
 
-      hsh["shipment_info_#{hospital_number}"] += objective_code_shipment_count
-      hsh["shipment_info"] += objective_code_shipment_count
-
-      hsh["objective_info_#{objective_code}_#{hospital_number}"] += objective_code_shipment_count
-      hsh["objective_info_#{objective_code}"] += objective_code_shipment_count
-
-      hsh["isolate_info_#{objective_code}_#{hospital_number}"] += objective_code_isolate_count
-      hsh["isolate_info_#{objective_code}"] += objective_code_isolate_count
-      hsh["isolate_info_total_#{hospital_number}"] += objective_code_isolate_count
-      hsh["isolate_info_total"] += objective_code_isolate_count
+      if hospital_number.blank? && objective_code.blank?
+        # the grand total for shipments and isolates row
+        hsh["shipment_info"]      = shipment_count
+        hsh["isolate_info_total"] = isolate_count
+      elsif objective_code.blank?
+        # a total row for shipments and isolates for a hospital
+        hsh["shipment_info_#{hospital_number}"]      = shipment_count
+        hsh["isolate_info_total_#{hospital_number}"] = isolate_count
+      elsif hospital_number.blank?
+        # a total row for shipments and isolates
+        hsh["objective_info_#{objective_code}"] = shipment_count
+        hsh["isolate_info_#{objective_code}"]   = isolate_count
+      else
+        hsh["objective_info_#{objective_code}_#{hospital_number}"] = shipment_count
+        hsh["isolate_info_#{objective_code}_#{hospital_number}"]   = isolate_count
+      end
     end
     hsh
   end
@@ -34,8 +40,7 @@ COUNT(DISTINCT shipments.id) AS objective_code_shipment_count,
 COUNT(isolates.*) AS objective_code_isolate_count
 FROM shipments
 LEFT OUTER JOIN isolates ON isolates.shipment_id = shipments.id
-GROUP BY 1,2
-ORDER BY 1,2
+GROUP BY CUBE( hospital_number, objective_code )
 END_OF_SQL
     ActiveRecord::Base.connection.select_rows sql
   end
